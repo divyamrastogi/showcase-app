c25f562a6d619dd1fb26c479e7433e6db837fd5a
Akash Hamirwasia <akash.hamirwasia@gmail.com>
2018-05-10 01:23:05 +0530

    Separated styles, and improved the code

---
+++ src/components/Button/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+import './Button.css';
+
+function CustomButton(props) {
+  const {
+    type = 'primary',
+    onClick,
+    className = '',
+    variant = 'flat',
+    children,
+  } = props;
+
+  return (
+    <Button
+      color={type}
+      variant={variant}
+      onClick={onClick}
+      className={`button ${type} ${className}`}  
+    >
+        { props.children ? props.children : 'Button' }
+    </Button>
+  )
+}
+
+export default CustomButton;
