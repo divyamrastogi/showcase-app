b48c740a1a042f7b5ccac423e39b773ced18cca7
Divyam Rastogi <divyamr@flock.com>
2018-05-16 18:32:17 +0530

    Add Prettier

--- src/DetailCard/DetailCard.js
+++ src/DetailCard/DetailCard.js
@@ -6,19 +6,19 @@ import Typography from 'material-ui/Typography';
 
 export class DetailCard extends Component {
 
-    constructor(props) {
-        super(props);
-        this.props = props;
-    }
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
 
-    render() {
-        return (
-            <div>
-                <Card className="detail-card">
-                    {this.props.children}
-                    {this.props.detail && <div className="more-info" > <a className="more-info-link" onClick={this.props.onClick}> {this.props.detailText} </a> </div>}
-                </Card>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Card className="detail-card">
+          {this.props.children}
+          {this.props.detail && <div className="more-info" > <a className="more-info-link" onClick={this.props.onClick}> {this.props.detailText} </a> </div>}
+        </Card>
+      </div>
+    );
+  }
 }
